ddbeb3c0f3e1dcbe62d018033a07fc8b40633c7a
yptian <1170492028@qq.com>
2026-01-06 21:48:51 +0800

    fixed: 配置打印

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -85,6 +85,10 @@ func setup() {
 	log.Infof("ExtConfig - Violet.DomainID: %d", ext.ExtConfig.Violet.DomainID)
 	log.Infof("ExtConfig - Upload.AppKey: %s", ext.ExtConfig.Upload.AppKey)
 	log.Infof("ExtConfig - Upload.Secret: %s", ext.ExtConfig.Upload.Secret)
+	log.Infof("ExtConfig - FrontendConfig.BaseSiteURL: %s", ext.ExtConfig.FrontendConfig.BaseSiteURL)
+	log.Infof("ExtConfig - FrontendConfig.BaseAPIURL: %s", ext.ExtConfig.FrontendConfig.BaseAPIURL)
+	log.Infof("ExtConfig - FrontendConfig.BaseH5URL: %s", ext.ExtConfig.FrontendConfig.BaseH5URL)
+	log.Infof("ExtConfig - FrontendConfig.BaseUploadURL: %s", ext.ExtConfig.FrontendConfig.BaseUploadURL)
 
 	usageStr := `starting api server...`
 	log.Info(usageStr)
