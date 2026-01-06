618de36d6cc697a1556c4adb291452d5da1672f3
yptian <1170492028@qq.com>
2026-01-06 21:57:28 +0800

    fixed: 配置

--- config/extend.go
+++ config/extend.go
@@ -24,10 +24,10 @@ const (
 //
 // 使用方法: config.ExtConfig......即可!!
 type Extend struct {
-	AMap           AMap           // 这里配置对应配置文件的结构即可
-	Violet         Violet         // Violet 反向代理配置
-	Upload         Upload         // Upload 上传服务配置
-	FrontendConfig FrontendConfig // 前端配置
+	AMap     AMap           // 这里配置对应配置文件的结构即可
+	Violet   Violet         // Violet 反向代理配置
+	Upload   Upload         // Upload 上传服务配置
+	frontend FrontendConfig // 前端配置
 }
 
 type AMap struct {
